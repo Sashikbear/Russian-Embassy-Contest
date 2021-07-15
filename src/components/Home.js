fd19d527d693098fcac79ef9b0750befc3988a7a
Alexandra Gritsenko <79204776+Sashikbear@users.noreply.github.com>
2021-07-15 14:17:21 +0300

    updating addresses

--- src/components/Home.js
+++ src/components/Home.js
@@ -54,7 +54,7 @@ function Home() {
 						<option value="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.0749712809434!2d34.77580681554478!3d32.067220927142316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4b7e9e3887f9%3A0x2a21f70d8eb9ed47!2sRothschild%20Blvd%2094%2C%20Tel%20Aviv-Yafo!5e0!3m2!1sen!2sil!4v1626339458770!5m2!1sen!2sil">
 							Тель-Авив
 						</option>
-						<option value="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4010.473701395956!2d34.84660557735654!3d32.30129502829056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4068fecd0019%3A0xa9f4b3bc4f20a5fa!2sVictory%20Monument!5e0!3m2!1sen!2sil!4v1626116669820!5m2!1sen!2sil">
+						<option value="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3370.9225362374764!2d34.85287934420261!3d32.34078909100953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d6ab0e8d0311b%3A0x7a0a5b0924e88ae7!2sNitsa%20Blvd%2026%2C%20Netanya!5e0!3m2!1sen!2sil!4v1626347648715!5m2!1sen!2sil">
 							Натания
 						</option>
 						<option value="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3353.1059992511323!2d34.990372215557294!3d32.815956289305255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151dbbb4faed49cd%3A0x5881a7815b4a1bba!2sHaGefen%20St%202%2C%20Haifa!5e0!3m2!1sen!2sil!4v1626339595436!5m2!1sen!2sil">
