dcc4665d0fcc597f2aceb41e7b6f16d2562c4254
Alexandra Gritsenko <79204776+Sashikbear@users.noreply.github.com>
2021-07-15 14:52:51 +0300

    mobile version footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,45 @@
 function Footer() {
 	return (
 		<div className="footer">
-			<div>
-				<i class="far fa-copyright"></i>Российский культурный центр в Тель-Авиве
+			<div className="footer-1">
+				<div>
+					<i class="far fa-copyright"></i>Российский культурный центр в
+					Тель-Авиве
+				</div>
+				<div>
+					<i class="fas fa-map-marker-alt"></i>Израиль, 6330440, г. Тель-Авив,
+					ул. Геула, д. 38
+				</div>
+				<div>
+					<a
+						style={{ textDecoration: "none", color: "#012f6a" }}
+						href="https://isr.rs.gov.ru/ru/contacts/48"
+					>
+						<i class="fas fa-at"></i>Контактная информация
+					</a>
+				</div>
 			</div>
-			<div>
-				<i class="fas fa-map-marker-alt"></i>Израиль, 6330440, г. Тель-Авив, ул.
-				Геула, д. 38
-			</div>
-			<div>
-				<a
-					style={{ textDecoration: "none", color: "#012f6a" }}
-					href="https://isr.rs.gov.ru/ru/contacts/48"
-				>
-					<i class="fas fa-at"></i>Контактная информация
-				</a>
-			</div>
-			<div>
-				<a href="https://www.facebook.com/russianhouse.tlv">
-					<i style={{ color: "#012f6a" }} class="fab fa-facebook"></i>
-				</a>
-			</div>
-			<div>
-				<a href="https://www.instagram.com/russianhouse.tlv/">
-					<i style={{ color: "#012f6a" }} class="fab fa-instagram"></i>
-				</a>
-			</div>
-			<div>
-				<a href="https://www.youtube.com/channel/UCtnzg9u7qezcJPfD1vaalUA">
-					<i style={{ color: "#012f6a" }} class="fab fa-youtube"></i>
-				</a>
-			</div>
-			<div>
-				<a href="https://twitter.com/russianhousetlv">
-					<i style={{ color: "#012f6a" }} class="fab fa-twitter"></i>
-				</a>
+			<div className="footer-2">
+				<div>
+					<a href="https://www.facebook.com/russianhouse.tlv">
+						<i style={{ color: "#012f6a" }} class="fab fa-facebook"></i>
+					</a>
+				</div>
+				<div>
+					<a href="https://www.instagram.com/russianhouse.tlv/">
+						<i style={{ color: "#012f6a" }} class="fab fa-instagram"></i>
+					</a>
+				</div>
+				<div>
+					<a href="https://www.youtube.com/channel/UCtnzg9u7qezcJPfD1vaalUA">
+						<i style={{ color: "#012f6a" }} class="fab fa-youtube"></i>
+					</a>
+				</div>
+				<div>
+					<a href="https://twitter.com/russianhousetlv">
+						<i style={{ color: "#012f6a" }} class="fab fa-twitter"></i>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
